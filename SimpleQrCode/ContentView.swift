80dbb10481ae4dafdbd9a74f5e066a10ad58b60f
0xC0FF33 <iwuerstchen@gmail.com>
2020-06-25 14:17:04 +0200

    Initial Commit

---
+++ SimpleQrCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleQrCode
+//
+//  Created by Martin Albrecht on 25.06.20.
+//  Copyright © 2020 Martin Albrecht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
